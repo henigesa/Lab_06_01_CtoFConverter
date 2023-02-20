f9bff68a14533aa913811e25c2f272c6f3bcbe62
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-20 00:14:40 -0500

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+
+/*
+class TempUnitConvert
+main ()
+num fahrenheit
+num celsius
+output “What is the temperature in Fahrenheit?”
+input fahrenheit
+celsius = (fahrenheit - 32) * 5/9
+output “The temperature in Celsius is” + celsius
+return
+end class
+ */
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        //declare variables
+        double fahr;
+        int celc;
+        //prompt user to enter the temperature
+        System.out.println("What is the temperature in Celsius?");
+        //take the user input
+        celc = input.nextInt();
+        //do math
+        fahr = (celc * 1.8) + 32 ;
+        System.out.println("The temperature in Fahrenheit is " + fahr );
     }
 }
