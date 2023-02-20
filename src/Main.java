f81f53c3de4962d1b57e79b55d5287892889b42b
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-20 00:52:25 -0500

    with if

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
         //declare variables
         double fahr;
-        int celc;
+        int celc = 0;
         //prompt user to enter the temperature
         System.out.println("What is the temperature in Celsius?");
         //take the user input
-        celc = input.nextInt();
+        if (input.hasNextInt()) {
+            celc = input.nextInt();
+            input.nextLine();
+        }
+        else {
+            System.out.println("That is not a valid input. Run the program and try again.");
+            System.exit(0);
+        }
         //do math
         fahr = (celc * 1.8) + 32 ;
         System.out.println("The temperature in Fahrenheit is " + fahr );
